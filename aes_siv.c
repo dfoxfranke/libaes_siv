7bc596b1936ed232f90809dec0f07b1249e1b143
Daniel Fox Franke <dafranke@akamai.com>
2017-02-23 15:42:23 -0500

    Add tests for 384- and 512-bit keys

--- aes_siv.c
+++ aes_siv.c
@@ -475,7 +475,7 @@ static void test_vector_1() {
         AES_SIV_CTX *ctx;
         int ret;
 
-        printf("Test case 1:\n");
+        printf("Test vector 1:\n");
         debug("key", key, sizeof key);
         debug("AD", ad, sizeof ad);
         debug("plaintext", plaintext, sizeof plaintext);
@@ -557,7 +557,7 @@ static void test_vector_2() {
         AES_SIV_CTX *ctx;
         int ret;
 
-        printf("Test case 2:\n");
+        printf("Test vector 2:\n");
         debug("key", key, sizeof key);
         debug("AD1", ad1, sizeof ad1);
         debug("AD2", ad2, sizeof ad2);
@@ -601,9 +601,147 @@ static void test_vector_2() {
 	AES_SIV_CTX_free(ctx);
 }
 
+static void test_384bit() {
+        const uint8_t key[] = {
+                0xff, 0xfe, 0xfd, 0xfc, 0xfb, 0xfa, 0xf9, 0xf8,
+                0xf7, 0xf6, 0xf5, 0xf4, 0xf3, 0xf2, 0xf1, 0xf0,
+                0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7,
+                0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff,
+                0xff, 0xfe, 0xfd, 0xfc, 0xfb, 0xfa, 0xf9, 0xf8,
+                0xf7, 0xf6, 0xf5, 0xf4, 0xf3, 0xf2, 0xf1, 0xf0
+        };
+
+        const uint8_t ad[] = {
+                0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+                0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
+                0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27
+        };
+
+        const uint8_t plaintext[] = {
+                0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,0x88,
+                0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee
+        };
+
+        const uint8_t ciphertext[] = {
+		0x89, 0xe8, 0x69, 0xb9, 0x32, 0x56, 0x78, 0x51,
+		0x54, 0xf0, 0x96, 0x39, 0x62, 0xfe, 0x07, 0x40,
+		0xef, 0xf3, 0x56, 0xe4, 0x2d, 0xec, 0x1f, 0x4f,
+		0xeb, 0xde, 0xd3, 0x66, 0x42, 0xf2
+	};
+
+        uint8_t ciphertext_out[256];
+        uint8_t plaintext_out[256];
+
+        size_t plaintext_len = sizeof plaintext_out;
+        size_t ciphertext_len = sizeof ciphertext_out;
+
+        AES_SIV_CTX *ctx;
+        int ret;
+
+        printf("384-bit key test:\n");
+        debug("key", key, sizeof key);
+        debug("AD", ad, sizeof ad);
+        debug("plaintext", plaintext, sizeof plaintext);
+        debug("exp. ciphertext", ciphertext, sizeof ciphertext);
+
+        ctx = AES_SIV_CTX_new();
+        assert(ctx != NULL);
+
+        printf("Encryption:\n");
+        ret = AES_SIV_Encrypt(ctx, ciphertext_out, &ciphertext_len,
+                              key, sizeof key, NULL, 0,
+                              plaintext, sizeof plaintext,
+                              ad, sizeof ad);
+        assert(ret == 1);
+	assert(ciphertext_len == sizeof ciphertext);
+	assert(!memcmp(ciphertext, ciphertext_out, ciphertext_len));
+
+        printf("Decryption:\n");
+        ret = AES_SIV_Decrypt(ctx, plaintext_out, &plaintext_len,
+                              key, sizeof key, NULL, 0,
+                              ciphertext_out, ciphertext_len,
+                              ad, sizeof ad);
+        assert(ret == 1);
+        assert(plaintext_len == sizeof plaintext);
+	assert(!memcmp(plaintext, plaintext_out, plaintext_len));
+	AES_SIV_CTX_cleanup(ctx);
+	AES_SIV_CTX_free(ctx);
+}
+
+static void test_512bit() {
+        const uint8_t key[] = {
+                0xff, 0xfe, 0xfd, 0xfc, 0xfb, 0xfa, 0xf9, 0xf8,
+                0xf7, 0xf6, 0xf5, 0xf4, 0xf3, 0xf2, 0xf1, 0xf0,
+                0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7,
+                0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff,
+                0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7,
+                0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff,
+                0xff, 0xfe, 0xfd, 0xfc, 0xfb, 0xfa, 0xf9, 0xf8,
+                0xf7, 0xf6, 0xf5, 0xf4, 0xf3, 0xf2, 0xf1, 0xf0
+        };
+
+        const uint8_t ad[] = {
+                0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+                0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
+                0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27
+        };
+
+        const uint8_t plaintext[] = {
+                0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,0x88,
+                0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee
+        };
+
+	const uint8_t ciphertext[] = {
+		0x72, 0x4d, 0xfb, 0x2e, 0xaf, 0x94, 0xdb, 0xb1,
+		0x9b, 0x0b, 0xa3, 0xa2, 0x99, 0xa0, 0x80, 0x1e,
+		0xf3, 0xb0, 0x5a, 0x55, 0x49, 0x8e, 0xc2, 0x55,
+		0x26, 0x90, 0xb8, 0x98, 0x10, 0xe4
+	};
+
+        uint8_t ciphertext_out[256];
+        uint8_t plaintext_out[256];
+
+        size_t plaintext_len = sizeof plaintext_out;
+        size_t ciphertext_len = sizeof ciphertext_out;
+
+        AES_SIV_CTX *ctx;
+        int ret;
+
+        printf("512-bit key test:\n");
+        debug("key", key, sizeof key);
+        debug("AD", ad, sizeof ad);
+        debug("plaintext", plaintext, sizeof plaintext);
+        debug("exp. ciphertext", ciphertext, sizeof ciphertext);
+
+        ctx = AES_SIV_CTX_new();
+        assert(ctx != NULL);
+
+        printf("Encryption:\n");
+        ret = AES_SIV_Encrypt(ctx, ciphertext_out, &ciphertext_len,
+                              key, sizeof key, NULL, 0,
+                              plaintext, sizeof plaintext,
+                              ad, sizeof ad);
+        assert(ret == 1);
+	assert(ciphertext_len == sizeof ciphertext);
+	assert(!memcmp(ciphertext, ciphertext_out, ciphertext_len));
+
+        printf("Decryption:\n");
+        ret = AES_SIV_Decrypt(ctx, plaintext_out, &plaintext_len,
+                              key, sizeof key, NULL, 0,
+                              ciphertext_out, ciphertext_len,
+                              ad, sizeof ad);
+        assert(ret == 1);
+        assert(plaintext_len == sizeof plaintext);
+	assert(!memcmp(plaintext, plaintext_out, plaintext_len));
+	AES_SIV_CTX_cleanup(ctx);
+	AES_SIV_CTX_free(ctx);
+}
+
 int main() {
         test_vector_1();
         test_vector_2();
+	test_384bit();
+	test_512bit();
         return 0;
 }
 #endif
