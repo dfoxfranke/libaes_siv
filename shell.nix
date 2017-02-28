250615591e02fd6654ceaa687bdacf7b7d85970c
Daniel Franke <dafranke@akamai.com>
2017-02-27 19:24:47 -0500

    Add cmake to nix dependences

--- shell.nix
+++ shell.nix
@@ -2,5 +2,5 @@
 with pkgs;
 stdenv.mkDerivation {
   name = "libaes_siv";
-  buildInputs = [ asciidoc openssl libxml2 libxslt docbook_xml_dtd_45 docbook_xml_xslt ];
+  buildInputs = [ cmake asciidoc openssl libxml2 libxslt docbook_xml_dtd_45 docbook_xml_xslt ];
 }
